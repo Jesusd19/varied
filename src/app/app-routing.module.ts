b337700bd0f24dd36cf16a2ee6f9f71eecc06506
Jesus Barajas <jesusdbarajas@gmail.com>
2019-03-20 16:13:48 -0500

    Agregando rutas hijas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,28 @@
-import { UserComponent } from './components/user/user.component';
-import { HomeComponent } from './components/home/home.component';
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { UserDetailsComponent } from "./components/user/user-details.component";
+import { UserEditComponent } from "./components/user/user-edit.component";
+import { UserComponent } from "./components/user/user.component";
+import { HomeComponent } from "./components/home/home.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { UserNewComponent } from "./components/user/user-new.component";
 
 const routes: Routes = [
-  { path:'home',  component: HomeComponent},
-  { path:'user', component: UserComponent},
-  { path: '**', pathMatch: 'full', redirectTo: 'home'}
+  { path: "home", component: HomeComponent },
+  {
+    path: "user/:id",
+    component: UserComponent,
+    children: [
+      { path: "new", component: UserNewComponent },
+      { path: "edit", component: UserEditComponent },
+      { path: "details", component: UserDetailsComponent },
+      { path: "**", pathMatch: "full", redirectTo: "new" }
+    ]
+  },
+  { path: "**", pathMatch: "full", redirectTo: "home" }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
