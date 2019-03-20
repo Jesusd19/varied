07d4e822546b1339483c6d98f99493039558203d
Jesus Barajas <jesusdbarajas@gmail.com>
2019-03-20 10:02:09 -0500

    Proceso asincrono

--- src/app/components/clases/clases.component.ts
+++ src/app/components/clases/clases.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-clases',
-  templateUrl: './clases.component.html',
+  selector: "app-clases",
+  templateUrl: "./clases.component.html",
   styles: []
 })
 export class ClasesComponent implements OnInit {
-  alert:string = "alert-primary";
+  alert: string = "alert-primary";
+  loading: boolean = false;
 
-  propiedades:Object = {
+  propiedades: Object = {
     danger: false
+  };
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  ejecutar() {
+    this.loading = true;
+    setTimeout(() => (this.loading = false), 3000);
   }
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
 }
