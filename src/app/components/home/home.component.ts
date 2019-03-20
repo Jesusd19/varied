43ed0d3f74781547fd5bde65f4f81e5c7eeab7f8
Jesus Barajas <jesusdbarajas@gmail.com>
2019-03-20 16:59:49 -0500

    Ciclo de vida de un componente

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -13,11 +13,44 @@ import { Component, OnInit } from '@angular/core';
   `,
   styles: []
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit  {
 
-  constructor() { }
+  constructor() { 
+    console.log("constructor");
+    
+  }
 
   ngOnInit() {
+    console.log("ngOnInit");
   }
+  ngOnChanges (){
+    console.log("OnChanges");
+    
+  }
+  ngDoCheck(){
+    console.log("DoCheck");
+    
+  }
+  ngAfterContentInit(){
+    console.log("AfterContentInit");
+    
+  }
+  ngAfterContentChecked (){
+    console.log("AfterContentChecked");
+    
+  }
+  ngAfterViewInit(){
+    console.log("AfterViewInit");
+    
+  }
+  ngAfterViewChecked(){
+    console.log("AfterViewChecked");
+    
+  }
+  ngOnDestroy(){
+    console.log("OnDestroy");
+    
+  }
+
 
 }
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styles: []
 })
-export class UserComponent implements OnInit {
+export class UserComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private activatedRoute: ActivatedRoute) { 
+    this.activatedRoute.params.subscribe( param => {
+      console.log("Ruta Padre");      
+      console.log(param);
+    })
   }
+
+  
 
 }
